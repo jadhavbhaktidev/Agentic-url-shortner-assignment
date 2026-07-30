fd168a7032ce4deff7760974a26a3e1529e33eda
Bhakti Jadhav <jadhavbhakti091@gmail.com>
2026-07-30 12:14:53 +0530

    added home controller

---
+++ src/test/java/com/agentic/urlshortener/controller/HomePageTest.java
@@ -0,0 +1,26 @@
+package com.agentic.urlshortener.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomePageTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void rootPage_shouldForwardToIndexHtml() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(forwardedUrl("/index.html"));
+    }
+}
